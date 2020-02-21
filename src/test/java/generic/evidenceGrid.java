bf5a2fcaff49c09ff97d00a5b06f947d1ac6f7f2
Leonardo Aguilar <leo.aguilar.gtz@gmail.com>
2020-02-21 13:08:08 -0600

    Cambio de colores en la evidencia

--- src/test/java/generic/evidenceGrid.java
+++ src/test/java/generic/evidenceGrid.java
@@ -353,8 +353,8 @@ public class evidenceGrid {
             printw.println("<head><title>Reporte de Evidencia</title>");
             
             printw.println("<style>");
-                printw.println(".accordion {");
-                  printw.println("background-color: #04B431;");
+                printw.println(".exitosoAccordion {");
+                  printw.println("background-color: #4CAF50;");
                   printw.println("color: #FFF;");
                   printw.println("cursor: pointer;");
                   printw.println("padding: 18px;");
@@ -392,7 +392,7 @@ public class evidenceGrid {
                   printw.println("transition: 0.4s;");
                 printw.println("}");
 
-                printw.println(".active, .accordion:hover {");
+                printw.println(".active, .exitosoAccordion:hover {");
                   printw.println("background-color: #ccc;");
                 printw.println("}");
                 
@@ -406,29 +406,29 @@ public class evidenceGrid {
 
                 
                 
-                printw.println(".accordion:after {");
+                printw.println(".exitosoAccordion:after {");
                   printw.println("content: '\002B';");
-                  printw.println("color: #777;");
+                  printw.println("color: #46a049;");
                   printw.println("font-weight: bold;");
                   printw.println("float: right;");
                   printw.println("margin-left: 5px;");
                 printw.println("}");
                 
-//                printw.println(".fallaAccordion:after {");
-//                  printw.println("content: '\002B';");
-//                  printw.println("color: #777;");
-//                  printw.println("font-weight: bold;");
-//                  printw.println("float: right;");
-//                  printw.println("margin-left: 5px;");
-//                printw.println("}");
-//                
-//                printw.println(".ejecucionAccordion:after {");
-//                  printw.println("content: '\002B';");
-//                  printw.println("color: #777;");
-//                  printw.println("font-weight: bold;");
-//                  printw.println("float: right;");
-//                  printw.println("margin-left: 5px;");
-//                printw.println("}");
+                printw.println(".fallaAccordion:after {");
+                  printw.println("content: '\002B';");
+                  printw.println("color: #da190b;");
+                  printw.println("font-weight: bold;");
+                  printw.println("float: right;");
+                  printw.println("margin-left: 5px;");
+                printw.println("}");
+                
+                printw.println(".ejecucionAccordion:after {");
+                  printw.println("content: '\002B';");
+                  printw.println("color: #e68a00;");
+                  printw.println("font-weight: bold;");
+                  printw.println("float: right;");
+                  printw.println("margin-left: 5px;");
+                printw.println("}");
 
                 printw.println(".active:after {");
                   printw.println("content: \"\2212\";");
@@ -581,9 +581,9 @@ public class evidenceGrid {
             for(int cont=0; cont<contador; cont++){
                 if((cont+1)<contador){
                     try{
-                        printw.println("<button class=\"accordion\">"+Pasos.get(cont)+"</button>");
+                        printw.println("<button class=\"exitosoAccordion\">"+Pasos.get(cont)+"</button>");
                     }catch(Exception e){
-                        printw.println("<button class=\"accordion\">Sin Paso</button>");
+                        printw.println("<button class=\"exitosoAccordion\">Sin Paso</button>");
                     }
                     printw.println("<div class=\"panel\">");
                     try{
@@ -608,9 +608,9 @@ public class evidenceGrid {
                     }
                     if("Exitoso".equals(Resultado.substring(0, 7))){
                         try{
-                            printw.println("<button class=\"accordion\">"+Pasos.get(cont)+"</button>");
+                            printw.println("<button class=\"exitosoAccordion\">"+Pasos.get(cont)+"</button>");
                         }catch(Exception e){
-                            printw.println("<button class=\"accordion\">Sin Paso</button>");
+                            printw.println("<button class=\"exitosoAccordion\">Sin Paso</button>");
                         }
                         printw.println("<div class=\"panel\">");
                         try{
@@ -641,7 +641,7 @@ public class evidenceGrid {
             printw.println("</div>");
             
             printw.println("<script>");
-            printw.println("var acc = document.getElementsByClassName(\"accordion\");");
+            printw.println("var acc = document.getElementsByClassName(\"exitosoAccordion\");");
             printw.println("var i;");
 
             printw.println("for (i = 0; i < acc.length; i++) {");
